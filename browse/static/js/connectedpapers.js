c2b0846e35b95eb088b85c2dc470013d7effe565
KanHar <knaan.harpaz@gmail.com>
2020-12-07 17:25:26 -0500

    Done

--- browse/static/js/connectedpapers.js
+++ browse/static/js/connectedpapers.js
@@ -6,9 +6,12 @@
   var $output = $('#connectedpapers-output');
   var arxivId = window.location.pathname.split('/').reverse()[0];
   var arxivIdToCPIdUrl = REST_ADDR + '?arxiv=' + arxivId;
-  var communicationErrorHtml = '<p>Communication with the connection papers server failed.</p>';
-  var idNotRecognizedHtml = '<p>ArxivID not recognized. Please try again in a few days.</p>';
+  var communicationErrorHtml = '<p>Oops, seems like communiation with the Connected Papers server is down.</p>';
+  var idNotRecognizedHtml = '<p>Seems like this paper is still not in our database. Please try again in a few days.</p>';
+  
+  
   $.get(arxivIdToCPIdUrl).done(translationResponse => {
+    console.log("AAA");
     if (translationResponse == null) {
       $output.html(idNotRecognizedHtml);
       return;
@@ -22,14 +25,17 @@
     }
 
     var versionsFetchUrl = REST_ADDR + '?versions=' + paperId;
+
+    console.log("BBB");
     
     $.get(versionsFetchUrl).done(versionsResponse => {
+      console.log("CCC");
       const NUMBER_OF_THUMBNAILS = 18;
       
       var graphUrl = CONNECTED_PAPERS_ADDR + 'main/' + paperId + '/graph';
-      var buildGraphLinkHtml = '<a href="' + graphUrl + '" target="_blank"><p>View graph for ' + title + '.</p></a>';
-      var seeGraphLinkHtml = '<a href="' + graphUrl + '" target="_blank"><p>View graph for ' + title + '.</p></a>';
-      var graphNotVisual = '<p>paper ' + title + ' not parsed yet. Please try again in a few days.</p>';
+      var buildGraphLinkHtml = '<a href="' + graphUrl + '" target="_blank"><p style="margin:0;">View graph for ' + title + '.</p></a>';
+      var seeGraphLinkHtml = '<a href="' + graphUrl + '" target="_blank"><p style="margin:0;">View graph for ' + title + '.</p></a>';
+      var graphNotVisual = '<p>Seems like ' + title + ' is still not in our database. Please try again in a few days.</p>';
 
       function getRandomInt(max) {
         return Math.floor(Math.random() * (max + 1));
@@ -39,11 +45,11 @@
       var choserGraphHtml = '<img src="' + chosenGraph + '" alt="Example graph image" width="120" height="100">';
 
       var containerDivStyle = '"display: flex; flex-flow: row; padding: 24px 10px;"';
-      var infoLine = '<p style="size: 12px; opacity: 0.5">Find and explore related papers in a visual graph</p>';
+      var infoLine = '<p style="font-size: 12px; opacity: 0.5; margin-top: 0; margin-bottom: 10px;">Find and explore related papers in a visual graph</p>';
 
-      var textDivOpen = '<div style="display: flex; flex-flow: column;">';
-      var buildGraphTextDiv = textDivOpen + infoLine + buildGraphLinkHtml '</div>';
-      var seeGraphTextDiv = textDivOpen + infoLine + seeGraphLinkHtml '</div>';
+      var textDivOpen = '<div style="display: flex; flex-flow: column; padding: 0px 25px;">';
+      var buildGraphTextDiv = textDivOpen + infoLine + buildGraphLinkHtml + '</div>';
+      var seeGraphTextDiv = textDivOpen + infoLine + seeGraphLinkHtml + '</div>';
 
       var buildGraphHtml = '<div style=' + containerDivStyle + '>' + choserGraphHtml + buildGraphTextDiv + '</div>';
       var seeGraphHtml = '<div style=' + containerDivStyle + '>' + choserGraphHtml + seeGraphTextDiv + '</div>';
