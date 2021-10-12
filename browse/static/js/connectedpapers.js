23529d85715e39acee172ba87d2e5ee42532ab26
Itay Knaan Harpaz <knaan.harpaz@gmail.com>
2021-10-13 01:16:40 +0300

    Revert "Debug"
    This reverts commit 5ed53e5447a506c195cccb5cbfc41ca055cc3dd9.

--- browse/static/js/connectedpapers.js
+++ browse/static/js/connectedpapers.js
@@ -107,7 +107,6 @@
         return;
       }
       const versionsData = versionsResponse.graph_versions;
-      console.log(versionsData);
       if (versionsData.length == 0) {
         // Graph not yet built ever
         $output.html(htmlPrefix + buildGraphHtml);
